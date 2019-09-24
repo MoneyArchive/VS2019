ba6ef7a2c0c890699a753540589079a1004033c2
Money Yu <abc12207@gmail.com>
2019-09-25 05:11:34 +0800

    add range and index demo

---
+++ 04.CSharpEight/CSharpEight/AsyncIteratorDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncIteratorDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Demo();
+        }
+
+        public static async Task Demo()
+        {
+            await foreach (var number in GenerateNum())
+            {
+                Console.WriteLine($"Time: {DateTime.Now:HH:mm:ss}. Get: {number}");
+            }
+        }
+
+        static async IAsyncEnumerable<int> GenerateNum()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                if (i % 10 == 0)
+                    await Task.Delay(5000);
+                yield return i;
+            }
+        }
+    }
+}
---
+++ 04.CSharpEight/CSharpEight/RangeDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RangeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new string[]
+            {
+                            // index from start    index from end
+                "Item1",    // 0                   ^9
+                "Item2",    // 1                   ^8
+                "Item3",    // 2                   ^7
+                "Item4",    // 3                   ^6
+                "Item5",    // 4                   ^5
+                "Item6",    // 5                   ^4
+                "Item7",    // 6                   ^3
+                "Item8",    // 7                   ^2
+                "Item9"     // 8                   ^1
+            };              // 9 (or words.Length) ^0
+
+            //foreach (var item in words[1..5])
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine($"The last word is {words[^1]}");
+
+            //Console.WriteLine(string.Concat(words[^2..^0]));
+
+            //Console.WriteLine(string.Concat(words[..])); 
+            //Console.WriteLine(string.Concat(words[..4])); 
+            //Console.WriteLine(string.Concat(words[6..])); 
+
+            Index a1 = 1;
+            Index a4 = 4;
+            //Console.WriteLine(string.Concat(words[a1..a4])); 
+        }
+    }
+}
