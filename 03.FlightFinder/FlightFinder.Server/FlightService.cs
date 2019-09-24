12da7efb4b44902f0b46283901992ee601b18513
Money Yu <abc12207@gmail.com>
2019-09-25 03:55:17 +0800

    update blazor scenario

---
+++ 03.FlightFinder/FlightFinder.Server/FlightService.cs
@@ -0,0 +1,17 @@
+﻿using FlightFinder.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightFinder.Server
+{
+    public class FlightService
+    {
+        public IEnumerable<Itinerary> ListSearchResult(SearchCriteria criteria)
+        {
+            // TODO: 晚點再做
+            return new List<Itinerary>();
+        }
+    }
+}
