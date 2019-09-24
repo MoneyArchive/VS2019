7ed2ebfe3700d932bd5b819eac50ab6254850ff8
Money Yu <abc12207@gmail.com>
2019-09-25 04:38:00 +0800

    add NullableTest

---
+++ 04.CSharpEight/CSharpEight/NullableTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NullableTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var money = new Person("Money", "Yu");
+            var len = GetMiddleNameLength(money);
+            Console.WriteLine(len);
+        }
+
+        static int GetMiddleNameLength(Person person)
+        {
+            var middle = person.MiddleName;
+            return middle.Length;
+        }
+    }
+}
